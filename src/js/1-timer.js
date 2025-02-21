3e107171bbc7e0d3cf49d502926de35d2a2e29e6
Yura Shaplavsky <yurikorock777@gmail.com>
2025-02-21 17:52:03 +0200

    flatpickr add

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -110,3 +110,15 @@
 //   captionsData: 'alt',
 //   captionDelay: 250,
 // });
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+flatpickr('#datetime-picker', {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+});
