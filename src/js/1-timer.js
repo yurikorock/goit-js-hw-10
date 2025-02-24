acd7a28178649b794d73f2c206fad35d3f12e2eb
Yura Shaplavsky <yurikorock777@gmail.com>
2025-02-24 15:53:43 +0200

    add timer and function of timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -116,9 +116,11 @@ import 'flatpickr/dist/flatpickr.min.css';
 let userSelectedDate;
 
 const btnStart = document.querySelector('[data-start]');
+const input = document.querySelector('#datetime-picker');
 // console.log(btnStart);
 btnStart.disabled = true;
 
+// перевірка валідної дати в календарі
 flatpickr('#datetime-picker', {
   enableTime: true,
   time_24hr: true,
@@ -128,7 +130,7 @@ flatpickr('#datetime-picker', {
     // console.log(selectedDates[0]);
     const selectedDate = selectedDates[0];
 
-    if (!selectedDate || selectedDate < Date.now()) {
+    if (!selectedDate || selectedDate.getTime() < Date.now()) {
       window.alert('"Please choose a date in the future"');
       btnStart.disabled = true;
       return;
@@ -138,3 +140,60 @@ flatpickr('#datetime-picker', {
     }
   },
 });
+
+//запуск таймера
+btnStart.addEventListener('click', () => {
+  btnStart.disabled = true;
+  input.disabled = true;
+
+  let timerId = setInterval(() => {
+    const currentTime = Date.now();
+    const deltaTime = userSelectedDate - currentTime;
+
+    if (deltaTime <= 0) {
+      clearInterval(timerId);
+      updateTimerUI({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      input.disabled = false;
+      return;
+    }
+
+    const timeComponents = convertMs(deltaTime);
+    updateTimerUI(timeComponents);
+  }, 1000);
+});
+
+// функція підрахунку значень
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+// функція оновлення інтерфейсу
+function updateTimerUI({ days, hours, minutes, seconds }) {
+  document.querySelector('[data-days]').textContent = addLeadingZero(days);
+  document.querySelector('[data-hours]').textContent = addLeadingZero(hours);
+  document.querySelector('[data-minutes]').textContent =
+    addLeadingZero(minutes);
+  document.querySelector('[data-seconds]').textContent =
+    addLeadingZero(seconds);
+}
+
+// функція форматує значення і додає ноль в інтерфейс
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
